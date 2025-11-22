65431782cb2f0de376e8ce61e1c7e33b67c2fac0
Saief Eddine Jouini <sayfjouini89@gmail.com>
2025-11-22 15:17:33 +0100

    Fix unused import issue and validate pre-commit hook

--- src/main.py
+++ src/main.py
@@ -2,9 +2,10 @@ from preprocess import load_time_series
 from models import forecast_combined
 from visualize import plot_forecast
 
+
 def main():
     series = load_time_series("data/gld_price_data.csv", column="GLD")
-    forecast, arima_res, garch_res = forecast_combined(series, (2,0,3), (1,1), 20)
+    forecast, arima_res, garch_res = forecast_combined(series, (2, 0, 3), (1, 1), 20)
 
     print("ARIMA Summary:")
     print(arima_res.summary())
@@ -13,6 +14,6 @@ def main():
 
     plot_forecast(series, forecast, 20)
 
+
 if __name__ == "__main__":
     main()
-
